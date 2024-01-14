a19d51be6dcc36037619b112cde736c478de120f
MohammadSr1381 <m.sarhanged1381@gmail.com>
2024-01-14 19:27:56 +0330

    forgot password feature

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,8 +10,19 @@ urlpatterns = [
     
     path('login/' , views.login , name='login'),
     path('logout/' , views.logout , name='logout'),
-    path('mtAccount/', views.myAccount , name='myAccount'),
+    
+    
+    path('myAccount/', views.myAccount , name='myAccount'),
+    
     path('patientDashboard/' , views.patientDashboard , name='patientDashboard'),
     path('clinicDashboard/' , views.clinicDashboard , name='clinicDashboard'),
     path('laboratoryDashboard/' , views.laboratoryDashboard , name='laboratoryDashboard'),
+    
+    path('activate/<uidb64>/<token>/' , views.activate , name = 'activate'),
+    
+    path('forgot_password' , views.forgot_password , name='forgot_password'),
+    path('reset_password_validate/<uidb64>/<token>' , views.reset_password_validate , name='reset_password_validate'),
+    path('reset_password' , views.reset_password , name='reset_password'),
+
+
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,15 @@
+from base64 import urlsafe_b64decode
 import email
 from email import message
+from os import error
+import re
 from django.shortcuts import render , redirect
 from django.http import HttpResponse
-from accounts.utils import detectUser
+from accounts.utils import detectUser, send_verification_email
+
+from django.utils.http import urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
 
 from clinics.forms import ClinicForm
 from clinics.models import Clinic
@@ -58,6 +65,12 @@ def registerUser(request):
             user = User.objects.create_user(first_name = first_name , last_name = last_name , phone_number = phone_number , email = email, password = password)
             user.role = User.PATIENT    
             user.save()
+            
+            #verify
+            mail_subject = 'لطفا اکانت خود را فعال کنید'
+            email_template = 'accounts/emails/account_verification_email.html'
+            send_verification_email(request,user,mail_subject,email_template)
+            
             messages.success(request , "your account has been saved successfully")
             print('user created successfully')
             return redirect('registerUser')
@@ -104,6 +117,12 @@ def registerClinic(request):
             
             clinic = Clinic(user = user , user_profile = user_profile , clinic_name = clinic_name , citizen_id = citizen_id , city = city , address = address , clinic_license = clinic_license)
             clinic.save()
+            
+            #verify
+            mail_subject = 'لطفا اکانت خود را فعال کنید'
+            email_template = 'accounts/emails/account_verification_email.html'
+            send_verification_email(request,user,mail_subject,email_template)   
+                     
             messages.success(request , "your account made successfully")
             return redirect('registerClinic')
             
@@ -153,6 +172,12 @@ def registerLaboratory(request):
             
             clinic = Clinic(user = user , user_profile = user_profile , clinic_name = clinic_name , citizen_id = citizen_id , city = city , address = address , clinic_license = clinic_license)
             clinic.save()
+            
+            #verify
+            mail_subject = 'لطفا اکانت خود را فعال کنید'
+            email_template = 'accounts/emails/account_verification_email.html'
+            send_verification_email(request,user,mail_subject,email_template)
+            
             messages.success(request , "your account made successfully")
             return redirect('registerClinic')
             
@@ -168,6 +193,29 @@ def registerLaboratory(request):
         'clinic_form' : clinic_form ,
     }
     return render(request , 'accounts/registerLaboratory.html' ,context)
+
+def activate(request , uidb64 , token):
+
+    try:
+        uid = urlsafe_base64_decode(uidb64)
+        user = User._default_manager.get(pk=uid)
+    except(TypeError , ValueError , OverflowError , User.DoesNotExist):
+        user = None
+    
+    if user is not None and default_token_generator.check_token(user , token):
+        user.is_active = True
+        user.save()
+        messages.success(request,'congrats ur account is activate')
+        return redirect('myAccount')
+    else:
+        
+        print(default_token_generator.check_token(user , token))
+        print('hello')
+        messages.error(request , 'invalid activation link :')
+        return redirect('myAccount')
+        
+    
+
 
 def login(request):
     if request.user.is_authenticated:
@@ -213,3 +261,64 @@ def clinicDashboard(request):
 @user_passes_test(check_role_Laboratory)
 def laboratoryDashboard(request):
     return render(request, 'accounts/laboratoryDashboard.html') 
+
+
+def forgot_password(request):
+    
+    if request.method == 'POST':
+        
+        email = request.POST['email']
+        if User.objects.filter(email=email).exists():
+            user = User.objects.get(email__exact=email)
+            
+            #send password reset email
+            mail_subject = 'تغییر رمز عبور' 
+            email_template = 'accounts/emails/reset_password_email.html'
+            send_verification_email(request,user,mail_subject,email_template)
+            
+            messages.success(request ,'password reset link has been sent to ur email')
+            return redirect('login')
+        else :
+            messages.success(request , 'account does not exist')
+            return redirect('forgot_password')
+        
+    return render (request , 'accounts/forgot_password.html')
+
+
+def reset_password_validate(request , uidb64 , token):
+    try:
+        uid = urlsafe_base64_decode(uidb64)
+        user = User._default_manager.get(pk=uid)
+    except(TypeError , ValueError , OverflowError , User.DoesNotExist):
+        user = None
+        
+    if user is not None and default_token_generator.check_token(user , token):
+            request.session['uid'] = uid
+            messages.info(request , 'please reset your password')
+            return redirect('reset_password')
+    else:
+        messages.error(request , 'this link has expired')
+        return redirect('myAccount')
+    
+
+def reset_password(request):
+    
+        if request.method == 'POST':
+            password = request.POST['password']
+            confirm_password = request.POST['confirm_password']
+            
+            if password == confirm_password:
+                pk = request.session.get('uid')
+                user = User.objects.get(pk=pk)
+                user.set_password(password)
+                user.is_active = True
+                user.save()
+                messages.success(request , 'password changed successfully')
+                return redirect('login')
+            else:
+                messages.error('passwords do not match')
+                return redirect('reset_password')
+            
+            
+        return render (request , 'accounts/reset_password.html')
+
